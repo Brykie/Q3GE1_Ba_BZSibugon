a4a2ea1dc4e89cf202f31b86b071aed16d992d44
Brykie <143463506+Brykie@users.noreply.github.com>
2024-04-05 14:52:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var Celsius = prompt("Please input a temperature value in Celsius (number only)");
+var Fahrenheit;
+
+Fahrenheit = ((Celsius * 9/5) + 32);
+
+alert("The temperature in Fahrenheit is " + Fahrenheit);
